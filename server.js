0343f973ed371045bbcefcad2a13c5c95e1301c2
karenjli <karenjy.li@gmail.com>
2019-10-07 18:11:47 -0500

    SQL portion completed

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
-
+const db = require("./data/dbConfig.js");
 const server = express();
 
 server.use(express.json());
+
+server.get("/", (req, res) => {
+  db("accounts");
+    .then(account => {
+      res.json(account);
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: "There is an error" });
+    });
+});
 
 module.exports = server;
